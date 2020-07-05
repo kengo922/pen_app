24af6129426611509e12b44ba77c69cc8e8586e2
kengo922 <kengo.matsuno.922@gmail.com>
2020-07-06 00:12:45 +0900

    Capistranoの自動デプロイ ができないため、修正の記述をおこなった
    Net::SSH::AuthenticationFailedが出るため再修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,10 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '3.115.64.82', user: 'kengo', roles: %w{app db web}
+server '3.115.64.82', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/myapp_key_rsa']
 
 # role-based syntax
 # ==================

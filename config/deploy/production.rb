ac73421f950bc130a13a1d39b77ed33a4d4859a2
kengo922 <kengo.matsuno.922@gmail.com>
2020-07-06 20:05:55 +0900

    production.rbを修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,13 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server ENV['SERVICE_NAME_PRODUCTION_IP'], port: 22, roles: [:app, :web, :db], primary: true
+server ENV['SERVICE_NAME_PRODUCTION_IP'], user: 'kengo', roles: [:app, :web, :db], primary: true
 
-set :ssh_options, {
-    forward_agent: true,
-    user: fetch(:user),
-    keys: ["#{ENV['SERVICE_NAME_PRODUCTION_SSH_KEY']}"]
-}
+set :ssh_options,  keys: ["#{ENV['SERVICE_NAME_PRODUCTION_SSH_KEY']}"]
 
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}

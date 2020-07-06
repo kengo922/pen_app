9f601f60ccf962bf08a0c7cf55e8d9e00d39283b
kengo922 <kengo.matsuno.922@gmail.com>
2020-07-06 20:10:31 +0900

    DBエラーにより自動デプロイの記述修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 server ENV['SERVICE_NAME_PRODUCTION_IP'], user: 'kengo', roles: [:app, :web, :db], primary: true
 
-set :ssh_options,  keys: ["#{ENV['SERVICE_NAME_PRODUCTION_SSH_KEY']}"]
+# set :ssh_options,  keys: ["#{ENV['SERVICE_NAME_PRODUCTION_SSH_KEY']}"]
 
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
